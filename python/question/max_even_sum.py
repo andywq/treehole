1a383ef6084628d51910522622f40cde2df39c53
Wang Qi <wangqi@qiniu.com>
2021-06-18 08:35:52 +0800

    add question <max_even_sum> solution

--- python/question/max_even_sum.py
+++ python/question/max_even_sum.py
@@ -3,3 +3,26 @@ sum K numbers from list A (length N),
 sum should be an even number and as large as possible
 if K > len(A) returns -1
 '''
+
+def is_even(n):
+    return 0 == n % 2
+
+def solution(A, K):
+    if K > len(A):
+        return -1
+
+
+    A.sort(key=lambda x: -x)
+    larger = A[:K]
+    smaller = A[K:]
+    result = sum(larger)
+
+    if is_even(result):
+        return result
+
+    for i in range(K - 1, -1 , -1):
+        for x in smaller:
+            if not is_even(larger[i] + x):
+                return result - larger[i] + x
+
+    return -1
